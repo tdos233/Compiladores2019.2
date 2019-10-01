7ae05f4fcdf4dd97d43d1f2e1d7b425b8254c4aa
tdos233 <40371596+tdos233@users.noreply.github.com>
2019-10-01 17:20:26 -0300

    add compilador.py

---
+++ compilador.py
@@ -0,0 +1,23 @@
+import lexico
+import string
+import array
+
+contaVazio = 0
+entrada = []
+vazias = []
+while True:
+    try:
+        contaVazio+=1
+        auxEntrada = input()
+        if not auxEntrada.strip():
+            vazias.append(contaVazio)
+        entrada.append(auxEntrada)
+    except (EOFError):
+        break
+
+marcaFinal = '\0'
+pulaLinha = '\n'
+x = pulaLinha.join(entrada)
+entrada.append(marcaFinal)
+lexer = lexico.Lexer(x, vazias)
+lexer.criaToken()
